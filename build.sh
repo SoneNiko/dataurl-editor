0dfcc0fceaa3b56a2324469e56dc44b512070c31
Nikolas Heise <heisenikolas@gmail.com>
2024-12-29 14:41:45 +0100

    added custom font to show spaces

--- build.sh
+++ build.sh
@@ -2,7 +2,18 @@
 
 mkdir tmp
 
-html-minifier ./src/index.html --file-ext html --collapse-whitespace --remove-comments --minify-css true --minify-js true --output ./tmp/index.html
+# generate the data url for the font data:font/woff2;base64,
+font_closure="data:font/woff2;base64,"
+font_data_url="$font_closure$(base64 -w 0 ./src/s.woff2)"
+echo "Font data url: $font_data_url"
+echo ""
+
+
+
+# replace "s.woff" with the data url for the font
+cat ./src/index.html | sed "s*s.woff2*$font_data_url*g" > ./tmp/pre_index.html
+
+html-minifier ./tmp/pre_index.html --file-ext html --collapse-whitespace --remove-comments --minify-css true --minify-js true --output ./tmp/index.html
 
 # Minify the index.html file
 minified_html=$(tr -d '\n' < tmp/index.html | tr -s ' ')
